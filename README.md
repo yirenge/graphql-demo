73e070ce344a40f072b75adb036b819d516d6843
brainee <zhengyong145@163.com>
2020-01-02 22:27:11 +0800

    a demo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 graphql-demo
+#### ref 
+https://blog.csdn.net/qq_41882147/article/details/82966783
--- schema.js
+++ schema.js
@@ -2,7 +2,10 @@ const {
     GraphQLSchema,
     GraphQLObjectType,
     GraphQLString,
+    GraphQLInt,
+    GraphQLBoolean
   } = require('graphql');
+
 const queryObj = new GraphQLObjectType({
   name: 'myFirstQuery',
   description: 'a hello world demo',
@@ -11,12 +14,51 @@ const queryObj = new GraphQLObjectType({
           name: 'a hello world query',
           description: 'a hello world demo',
           type: GraphQLString,
+          args: {
+              name: {  // 这里定义参数，包括参数类型和默认值
+                  type: GraphQLString,
+                  defaultValue: 'Brian'
+              }
+          },
+          resolve(parentValue, args, request) { // 这里演示如何获取参数，以及处理
+              return 'hello world ' + args.name + '!';
+          }
+      },
+      person: {
+          name: 'personQuery',
+          description: 'query a person',
+          type: new GraphQLObjectType({ // 这里定义查询结果包含name,age,sex三个字段，并且都是不同的类型。
+              name: 'person',
+              fields: {
+                name: {
+                  type: GraphQLString
+                },
+                age: {
+                  type: GraphQLInt
+                },
+                sex: {
+                  type: GraphQLBoolean
+                }
+              }
+          }),
+          args: {
+              name: {
+                  type: GraphQLString,
+                  defaultValue: 'Brainee'
+              }
+          },
           resolve(parentValue, args, request) {
-              return 'hello world !';
+              return {
+                  name: args.name,
+                  age: args.name.length,
+                  sex: Math.random() > 0.5,
+                  test:2
+              };
           }
       }
   }
 });
+
 module.exports = new GraphQLSchema({
-query: queryObj
+query: queryObj 
 });
